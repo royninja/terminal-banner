33b3f3ae9c6a3cb0d422ed0d6796ae744bb40eb4
whoami0342 <roy.sayan.fb98@gmail.com>
2017-02-27 00:56:26 +0530

    First Commit

---
+++ terminal-banner.sh
@@ -0,0 +1,25 @@
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='033[0M'
+clear
+COUNTER=0
+	
+         while [  $COUNTER -lt 100 ]; do
+		echo "          banner created by sayan roy        "
+		echo " [*] initializing all the module... $COUNTER%"
+		sleep 1 && clear
+                let COUNTER=COUNTER+10
+	      
+         done
+clear
+printf "${GREEN} www       www	hh   hh   ooooooo	aaaaaa	mmmmmmmmm   iiiii\n"
+printf "${GREEN} www       www  hh   hh	  oo   oo	aa  aa  mm  m  mm    iii\n"	
+printf "${GREEN} www       www  hh   hh	  oo   oo	aa  aa  mm  m  mm    iii\n"	
+printf "${GREEN} www       www 	hhhhhhh	  oo   oo	aaaaaa	mm  m  mm    iii\n"	
+printf "${GREEN} www  www  www 	hh   hh	  oo   oo	aa  aa  mm     mm    iii\n"	
+printf "${GREEN} www wwwww www  hh   hh	  oo   oo	aa  aa  mm     mm    iii\n"	
+printf "${GREEN} wwwww    wwww  hh   hh   ooooooo	aa  aa	mm     mm   iiiii\n"
+echo ""
+echo ""
+printf "			---${YELLOW} sayan(${BLUE} power of linux terminal)\n"
